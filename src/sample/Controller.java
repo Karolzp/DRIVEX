fc600f163881b37150e4dddb702f9f8f1ca7326a
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2018-11-26 13:08:33 +0100

    controller class cleaned and ready for new project drivex2

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,29 @@
 package sample;
 
+import javafx.animation.PathTransition;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.scene.shape.*;
+import javafx.util.Duration;
+
 
 import java.net.URL;
+import java.security.AllPermission;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+
+
     }
+
+
 }
