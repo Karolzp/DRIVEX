cd1fadf8f594e5853dd62d3aa77a9b87d22be98b
Karolzp <zupnik.karol@gmail.com>
2018-11-28 10:56:20 +0100

    add traffic lights model

--- src/Lights/LightsModel.java
+++ src/Lights/LightsModel.java
@@ -1,4 +1,56 @@
 package Lights;
 
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
+import java.util.HashMap;
+
 public class LightsModel {
+
+    private boolean isGreen;
+    private HashMap<String,Integer> positionOfTrafficLightBox;
+    private Rectangle trafficLightsBox;
+    private Circle redLight;
+    private Circle greenLight;
+
+    public LightsModel(boolean isGreen, HashMap<String, Integer> positionOfTrafficLightBox) {
+        this.isGreen = isGreen;
+        this.positionOfTrafficLightBox = positionOfTrafficLightBox;
+        createTrafficLightBox();
+        createTrafficLightsCircles();
+    }
+
+    public HashMap<String, Integer> getPositionOfTrafficLightBox() {
+        return positionOfTrafficLightBox;
+    }
+
+
+    public boolean isGreen() {
+        return isGreen;
+    }
+
+    public void setGreen(boolean green) {
+        isGreen = green;
+    }
+
+    private void createTrafficLightBox(){
+        this.trafficLightsBox = new Rectangle();
+        trafficLightsBox.setX(positionOfTrafficLightBox.get("x"));
+        trafficLightsBox.setY(positionOfTrafficLightBox.get("y"));
+        trafficLightsBox.setHeight(positionOfTrafficLightBox.get("h"));
+        trafficLightsBox.setWidth(positionOfTrafficLightBox.get("w"));
+    }
+
+    private void createTrafficLightsCircles(){
+        this.greenLight = new Circle();
+        this.redLight = new Circle();
+
+        greenLight.setCenterX(positionOfTrafficLightBox.get("x")+(positionOfTrafficLightBox.get("w")/2));
+        greenLight.setCenterY(positionOfTrafficLightBox.get("y")+(positionOfTrafficLightBox.get("h")/3));
+        greenLight.setRadius(positionOfTrafficLightBox.get("w")/3);
+
+        redLight.setCenterX(positionOfTrafficLightBox.get("x")+(positionOfTrafficLightBox.get("w")/2));
+        redLight.setCenterY(positionOfTrafficLightBox.get("y")+(positionOfTrafficLightBox.get("h")/3*2));
+        redLight.setRadius(positionOfTrafficLightBox.get("w")/3);
+    }
 }
