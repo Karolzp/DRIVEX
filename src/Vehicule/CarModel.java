bcf9c0e88d65c917c90e64ca8a9ea9f36e2448d4
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2018-11-28 09:48:38 +0100

    roads ready in MVC structure

---
+++ src/Vehicule/CarModel.java
@@ -0,0 +1,78 @@
+package Vehicule;
+
+public class CarModel extends Vehicule {
+//    private int speedMax;
+//    private int speedActual;
+//    private String nameOfRoadWithThisCar;
+//    private int positionX;
+//    private int positionY;
+//    private int widthOfCar;
+//    private int heightOfCar;
+//
+//    public CarModel(int speedMax, int speedActual, int positionX, int positionY, int widthOfCar, int heightOfCar) {
+//        this.speedMax = speedMax;
+//        this.speedActual = speedActual;
+//        this.positionX = positionX;
+//        this.positionY = positionY;
+//        this.widthOfCar = widthOfCar;
+//        this.heightOfCar = heightOfCar;
+//    }
+//
+//    public int getSpeedMax() {
+//        return speedMax;
+//    }
+//
+//    public void setSpeedMax(int speedMax) {
+//        this.speedMax = speedMax;
+//    }
+//
+//    public int getSpeedActual() {
+//        return speedActual;
+//    }
+//
+//    public void setSpeedActual(int speedActual) {
+//        this.speedActual = speedActual;
+//    }
+//
+//    public int getPositionX() {
+//        return positionX;
+//    }
+//
+//    public void setPositionX(int positionX) {
+//        this.positionX = positionX;
+//    }
+//
+//    public int getPositionY() {
+//        return positionY;
+//    }
+//
+//    public void setPositionY(int positionY) {
+//        this.positionY = positionY;
+//    }
+//
+//    public int getWidthOfCar() {
+//        return widthOfCar;
+//    }
+//
+//    public void setWidthOfCar(int widthOfCar) {
+//        this.widthOfCar = widthOfCar;
+//    }
+//
+//    public int getHeightOfCar() {
+//        return heightOfCar;
+//    }
+//
+//    public void setHeightOfCar(int heightOfCar) {
+//        this.heightOfCar = heightOfCar;
+//    }
+//
+//    @Override
+//    protected void slowDown() {
+//
+//    }
+//
+//    @Override
+//    protected void speedUp() {
+//
+//    }
+}
--- src/Vehicule/Vehicule.java
+++ src/Vehicule/Vehicule.java
@@ -1,16 +1,16 @@
 package Vehicule;
 
 public abstract class Vehicule {
-
-    private int speedMax;
-    private int speedActual;
-    private String nameOfRoadWithThisCar;
-    private int positionX;
-    private int positionY;
-    private int widthOfCar;
-    private int heightOfCar;
-
-    protected abstract void slowDown();
-    protected abstract void speedUp();
+//
+//    private int speedMax;
+//    private int speedActual;
+//    private String nameOfRoadWithThisCar;
+//    private int positionX;
+//    private int positionY;
+//    private int widthOfCar;
+//    private int heightOfCar;
+//
+//    protected abstract void slowDown();
+//    protected abstract void speedUp();
 
 }
